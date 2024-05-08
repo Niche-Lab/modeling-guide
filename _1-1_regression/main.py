66d2c92de0daa13482cbc4b2d409f42826c089af
poissonfish <niche@vt.edu>
2024-05-08 04:05:53 -0400

    final draft

--- _1-1_regression/main.py
+++ _1-1_regression/main.py
@@ -2,6 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import r2_score
+import pandas as pd
+
+
+def rmse(x, y):
+    return np.sqrt(np.mean((x - y) ** 2))
+
+
+def rmspe(x, y):
+    return np.sqrt(np.mean(((x - y) / x) ** 2))
 
 
 def pearson_r(x, y):
@@ -14,10 +23,6 @@ def pearson_r(x, y):
     return r
 
 
-def rmse(x, y):
-    return np.sqrt(np.mean((x - y) ** 2))
-
-
 def determination_r2(obs, pred):
     obs_mu = np.mean(obs)
     ssr = np.sum((obs - pred) ** 2)
@@ -25,15 +30,45 @@ def determination_r2(obs, pred):
     return 1 - ssr / sst
 
 
+# ccc correlation
+def CCC(y_true, y_pred):
+    """Concordance correlation coefficient."""
+    # Raw data
+    dct = {"y_true": y_true, "y_pred": y_pred}
+    df = pd.DataFrame(dct)
+    # Remove NaNs
+    df = df.dropna()
+    # Pearson product-moment correlation coefficients
+    y_true = df["y_true"]
+    y_pred = df["y_pred"]
+    cor = np.corrcoef(y_true, y_pred)[0][1]
+    # Means
+    mean_true = np.mean(y_true)
+    mean_pred = np.mean(y_pred)
+    # Population variances
+    var_true = np.var(y_true)
+    var_pred = np.var(y_pred)
+    # Population standard deviations
+    sd_true = np.std(y_true)
+    sd_pred = np.std(y_pred)
+    # Calculate CCC
+    numerator = 2 * cor * sd_true * sd_pred
+    denominator = var_true + var_pred + (mean_true - mean_pred) ** 2
+
+    return numerator / denominator
+
+
 def print_status(x, y):
-    print(f"Correlation r = {pearson_r(x, y):.2f}")
-    print(f"Numpy Correlation r2 = {np.corrcoef(x, y)[0, 1]**2:.2f}")
-    print(f"Determination r2 = {determination_r2(x, y):.2f}")
-    print(f"Sklearn r2 = {r2_score(x, y):.2f}")
     print(f"RMSE = {rmse(x, y):.2f}")
+    print(f"RMSPE = {rmspe(x, y):.2f}")
+    print(f"Correlation r = {pearson_r(x, y):.2f}")
+    # print(f"Numpy Correlation r2 = {np.corrcoef(x, y)[0, 1]**2:.2f}")
+    print(f"Determination r2 = {determination_r2(x, y):.2f}")
+    # print(f"Sklearn r2 = {r2_score(x, y):.2f}")
+    print(f"CCC = {CCC(x, y):.2f}")
 
 
-RCORR = 0.3
+RCORR = 0.6
 NOISE = 1 - RCORR
 BLOCK_EFF = 3
 SEED = 24061
@@ -45,16 +80,16 @@ obs = np.concatenate(
         np.random.normal(-BLOCK_EFF, 1, 50),
     ]
 )
-pred_1 = obs * RCORR + np.random.normal(0, NOISE, 100)
+pred_1 = obs + np.random.normal(0, 1, 100)
 pred_2 = pred_1 * 5
-pred_3 = pred_1**5
-pred_3[np.abs(pred_3) < 16.7] = 0
+pred_3 = pred_1**3
+pred_3[np.abs(pred_3) < np.quantile(pred_3, 0.95)] = 0
 pred_4 = []
 for i, o in enumerate(obs):
     if o > 0:
-        pred_4 += [pred_1[i] + np.random.normal(BLOCK_EFF, NOISE)]
+        pred_4 += [pred_1[i] + np.random.normal(BLOCK_EFF, 2)]
     else:
-        pred_4 += [pred_1[i] + np.random.normal(-BLOCK_EFF, NOISE)]
+        pred_4 += [pred_1[i] + np.random.normal(-BLOCK_EFF, 2)]
 pred_4 = np.array(pred_4)
 
 SIZE = 100
@@ -86,12 +121,20 @@ sns.scatterplot(
     x=obs,
     y=pred_4,
     ax=axes[1, 1],
-    hue=obs > 0,
+    style=obs > 0,
+    color="grey",
     s=SIZE,
 )
 # remove legend
 axes[1, 1].get_legend().remove()
 
+
+print_status(obs, pred_1)
+print_status(obs, pred_2)
+print_status(obs, pred_3)
+print_status(obs, pred_4)
+print_status(obs[obs > 0], pred_4[obs > 0])
+print_status(obs[obs < 0], pred_4[obs < 0])
 
 fig.savefig("regression.png", dpi=300)
 
